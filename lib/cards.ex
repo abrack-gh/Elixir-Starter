06036892205b4f8b5b7a467e7690845fde432afd
abrack-gh <a.brack@live.co.uk>
2023-08-15 13:39:06 +0100

    List standardLibrary

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,13 +1,32 @@
 defmodule Cards do
 
   def create_deck do
-    ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
+
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
   "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+
+    suits = ["Spaces", "Clubs", "Hearts", "Diamonds"]
+
+
+    #For every element in suits, do suit - i.e. for loop
+    for suit <-  suits, value <- values do
+        "#{value} of #{suit}"
+
+    end
+
+
   end
 
   def shuffle(deck) do
 
     Enum.shuffle(deck)
+
+  end
+
+  #Check if a deck contains a card
+  def contains?(deck, card) do
+
+    Enum.member?(deck, card)
 
   end
 
